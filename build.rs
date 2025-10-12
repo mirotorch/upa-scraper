933b7f14138ece6024d52ed74a56f9b286162670
mirotorch <rkovalenko.m5@gmail.com>
2025-10-12 18:24:51 +0200

    custom debug build

---
+++ build.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let profile = env::var("PROFILE").unwrap();
+    if profile == "debug" {
+        let source_path = Path::new("test_page.html");
+        let dest_dir = Path::new(&out_dir)
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap();
+
+        let dest_path = dest_dir.join("test_page.html");
+
+        fs::copy(&source_path, &dest_path).expect("Failed to copy test_page.html");
+
+        println!("cargo:rerun-if-changed=test_page.html");
+    }
+}
